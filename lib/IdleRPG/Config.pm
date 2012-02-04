90bf331088e9719b8af19bf6f97e1e90a6a69c65
milki <milki@rescomp.berkeley.edu>
2012-02-03 16:15:06 -0800

    Move config to package

---
+++ lib/IdleRPG/Config.pm
@@ -0,0 +1,44 @@
+package IdleRPG::Config;
+
+use strict;
+use warnings;
+
+use AppConfig qw(:expand :argcount);
+
+my $CONFFILE = 'lib/IdleRPG/irpg.conf';
+
+my $config = AppConfig->new({
+            CASE    => 1,
+            GLOBAL  => {
+                DEFAULT     => undef,
+                ARGCOUNT    => ARGCOUNT_ONE,
+            },
+        });
+
+my $init = 0;
+
+my @EXPORT_OK = qw(get_config);
+my %EXPORT_TAGS = ( config => [qw(get_config)] );
+
+sub get_config {
+    _init() unless $init;
+    return $config;
+}
+
+sub _init {
+    unless($init) {
+        $config->define(
+            'baseurl',
+            'logo',
+            'db',
+            'botname',
+            'server',
+            'botchan',
+        );
+        $config->file($CONFFILE);
+
+        $init = 1;
+    }
+}
+
+1;
